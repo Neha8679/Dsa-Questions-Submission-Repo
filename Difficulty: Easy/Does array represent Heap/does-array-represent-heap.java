1e517af81855d7027a6114bcf004931a82b1136b
Neha8679 <safercode067@gmail.com>
2025-06-27 00:28:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Does array represent Heap/does-array-represent-heap.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+
+class Solution {
+
+    public boolean countSub(long arr[], long n) {
+        // Your code goes here
+        for(int i=0;i<=n/2;i++){
+            if((2 * i + 1 < n && arr[i] < arr[2 * i + 1]) || (2 * i + 2 < n && arr[i] < arr[2 * i + 2]))
+            {
+                return false;
+            }
+        }
+        return true;
+       
+    }
+}
