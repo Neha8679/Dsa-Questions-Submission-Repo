e60ae192ccc2c31770f9fdee06b1b38f9c62e91d
Neha8679 <safercode067@gmail.com>
2025-06-30 21:48:00 +0530

    Time: 52 ms (19.13%) | Memory: 62.9 MB (24.81%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,15 +1,36 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int[] ans=arr.clone();
-        Arrays.sort(arr);
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<ans.length;i++){
-            map.putIfAbsent(arr[i],map.size()+1);
+        // int[] ans=arr.clone();
+        // Arrays.sort(arr);
+        // Map<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<ans.length;i++){
+        //     map.putIfAbsent(arr[i],map.size()+1);
+        // }
+        // for(int i=0;i<ans.length;i++){
+        //     ans[i]=map.get(ans[i]);
+        // }
+        // return ans;
+
+        int n=arr.length;
+        PriorityQueue <int[]> pq= new PriorityQueue<>(new Comparator<int[]>(){
+            public int compare(int[]x,int[]y){
+                return x[0]-y[0];
+            }
+        });
+        for(int i=0;i<n;i++){
+            pq.offer(new int[]{arr[i],i});
         }
-        for(int i=0;i<ans.length;i++){
-            ans[i]=map.get(ans[i]);
+        int rank=0;
+        int prev=Integer.MAX_VALUE;
+        while(!pq.isEmpty()){
+            int cur[]=pq.poll();
+            if(prev!=cur[0]){
+                prev=cur[0];
+                rank++;
+            }
+            arr[cur[1]]=rank;
         }
-        return ans;
+        return arr;
         
     }
 }
