79cefce133851ac036297457efe749f7361dbdf1
Neha8679 <safercode067@gmail.com>
2025-06-30 15:31:44 +0530

    Time: 2 ms (87.27%) | Memory: 46.1 MB (5.39%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists==null || lists.length==0){
+            return null;
+        }
+
+        int interval=1;
+        while(interval<lists.length){
+            for(int i=0;i+interval<lists.length;i=i+interval*2){
+                lists[i]=mergeTwoLists(lists[i],lists[i+interval]);
+            }
+            interval=interval*2;
+        }
+        return lists[0];
+
+        
+    }
+    public ListNode mergeTwoLists(ListNode l1,ListNode l2){
+        if(l1==null){
+            return l2;
+        }
+        if(l2==null){
+            return l1;
+        }
+        ListNode newNode=new ListNode(l1.val);
+        ListNode temp=newNode;
+        while(l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                temp.next=new ListNode(l1.val);
+                l1=l1.next;
+
+
+            }
+            else{
+                temp.next=new ListNode(l2.val);
+                l2=l2.next;
+            }
+            temp=temp.next;
+        }
+        if(l1!=null){
+            temp.next=l1;
+        }
+        else{
+            temp.next=l2;
+        }
+        return newNode.next;
+    }
+}
