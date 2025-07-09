3c9aa46467c59a8ed8cad17b9e3151fe06c930c1
Neha8679 <safercode067@gmail.com>
2025-07-10 00:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (88.06%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -15,30 +15,38 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        List<List<Integer>> ans=new ArrayList<>();
-        if(root==null){
-            return 0;
+        // List<List<Integer>> ans=new ArrayList<>();
+        // if(root==null){
+        //     return 0;
 
-        }
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int n=q.size();
-            List<Integer> arr=new ArrayList<>();
-            for(int i=0;i<n;i++){
-                TreeNode node=q.poll();
-                arr.add(node.val);
-                if(node.left!=null){
-                    q.add(node.left);
+        // }
+        // Queue<TreeNode> q=new LinkedList<>();
+        // q.add(root);
+        // while(!q.isEmpty()){
+        //     int n=q.size();
+        //     List<Integer> arr=new ArrayList<>();
+        //     for(int i=0;i<n;i++){
+        //         TreeNode node=q.poll();
+        //         arr.add(node.val);
+        //         if(node.left!=null){
+        //             q.add(node.left);
 
-                }
-                if(node.right!=null){
-                    q.add(node.right);
-                }
-            }
-            ans.add(arr);
-        }
-        int x=ans.size();
-        return x;
+        //         }
+        //         if(node.right!=null){
+        //             q.add(node.right);
+        //         }
+        //     }
+        //     ans.add(arr);
+        // }
+        // int x=ans.size();
+        // return x;
+         
+
+         
+        if(root==null) return 0;
+        int l=maxDepth(root.left);
+        int r=maxDepth(root.right);
+        return 1+Math.max(l,r);
+    
     }
 }
