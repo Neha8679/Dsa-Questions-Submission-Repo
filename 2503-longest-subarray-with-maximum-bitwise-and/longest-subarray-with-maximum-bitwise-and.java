de1f1d0cd16b3f9dcdca9f958c433ae60cb3fbd8
Neha8679 <safercode067@gmail.com>
2025-07-30 22:57:54 +0530

    Time: 3 ms (82.57%) | Memory: 59.5 MB (85.89%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
@@ -1,25 +1,45 @@
+// class Solution {
+//     public int longestSubarray(int[] nums) {
+//         int n=nums.length;
+//         int max_value=0;
+//         int ans=0;
+//         int current_streak=0;
+//         for(int i=0;i<n;i++){
+//             if(max_value<nums[i]){
+//                 max_value=nums[i];
+//                 ans=current_streak=0;
+//             }
+//             if(max_value==nums[i]){
+//                 current_streak++;
+//             }
+//             else{
+//                 current_streak=0;
+//             }
+//             ans=Math.max(ans,current_streak);
+//         }
+//         return ans;
+
+        
+        
+//     }
+// }
+
+
 class Solution {
     public int longestSubarray(int[] nums) {
-        int n=nums.length;
-        int max_val=0;
-        int current_streak=0;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(max_val<nums[i]){
-                max_val=nums[i];
-                ans=current_streak=0;
-            }
-            if(max_val==nums[i]){
-                current_streak++;
-
-            }
-            else{
-                current_streak=0;
-            }
-            ans=Math.max(ans,current_streak);
-
+        int maxVal = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num > maxVal) maxVal = num;
         }
-        return ans;
-        
+        int streak = 0, maxStreak = 0;
+        for (int num : nums) {
+            if (num == maxVal) {
+                streak++;
+                if (streak > maxStreak) maxStreak = streak;
+            } else {
+                streak = 0;
+            }
+        }
+        return maxStreak;
     }
 }
