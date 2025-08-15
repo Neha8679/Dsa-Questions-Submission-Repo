259da75dfcf9c65548226911e7ff82336a0ac252
Neha8679 <safercode067@gmail.com>
2025-08-15 11:16:15 +0530

    Time: 3 ms (79.31%) | Memory: 42.7 MB (52.76%) - LeetSync

--- 3567-convert-date-to-binary/convert-date-to-binary.java
+++ 3567-convert-date-to-binary/convert-date-to-binary.java
@@ -1,23 +1,39 @@
-class Solution {
-    public String convertDateToBinary(String date) {
-        String[] parts=date.split("-");
-        int year=Integer.parseInt(parts[0]);
-        int month=Integer.parseInt(parts[1]);
-        int data=Integer.parseInt(parts[2]);
-        String yearBin=toBinary(year);
-        String monthBin=toBinary(month);
-        String dateBin=toBinary(data);
-        return yearBin + "-" +monthBin + "-" + dateBin;
+// class Solution {
+//     public String convertDateToBinary(String date) {
+//         String[] parts=date.split("-");
+//         int year=Integer.parseInt(parts[0]);
+//         int month=Integer.parseInt(parts[1]);
+//         int data=Integer.parseInt(parts[2]);
+//         String yearBin=toBinary(year);
+//         String monthBin=toBinary(month);
+//         String dateBin=toBinary(data);
+//         return yearBin + "-" +monthBin + "-" + dateBin;
 
         
-    }
-    public String toBinary(int x){
-        String ans="";
-        while(x!=0){
-            ans=x%2+ans;
-            x=x/2;
-        }
+//     }
+//     public String toBinary(int x){
+//         String ans="";
+//         while(x!=0){
+//             ans=x%2+ans;
+//             x=x/2;
+//         }
         
-        return ans;
+//         return ans;
+//     }
+// }
+
+
+class Solution {
+    public String convertDateToBinary(String date) {
+        StringBuilder sb = new StringBuilder();
+        String[] arr = date.split("-");
+
+        for (String s : arr) {
+           sb.append(Integer.toBinaryString(Integer.parseInt(s)));
+           sb.append("-");
+        }
+
+        sb.setLength(sb.length() - 1);
+        return sb.toString();
     }
 }
