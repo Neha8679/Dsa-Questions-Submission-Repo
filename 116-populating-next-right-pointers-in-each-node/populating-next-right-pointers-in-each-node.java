9bf0cec30f8a7a0dc865ff14c6554cadd7a19e84
Neha8679 <safercode067@gmail.com>
2025-08-15 14:51:52 +0530

    Time: 2 ms (53.18%) | Memory: 44.6 MB (33.37%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -21,23 +21,55 @@ class Node {
 };
 */
 
+// class Solution {
+//     public Node connect(Node root) {
+//         if(root == null) return null;
+//         Queue<Node> q = new LinkedList<>();
+//         q.offer(root);
+//         while(!q.isEmpty()) {
+//             Node rightNode = null;
+//             for(int i = q.size(); i > 0; i--) {
+//                 Node cur = q.poll();
+//                 cur.next = rightNode;
+//                 rightNode = cur;
+//                 if(cur.right != null) {
+//                     q.offer(cur.right);
+//                     q.offer(cur.left);
+//                 }
+//             }
+//         }
+//         return root;        
+//     }
+// }
+
+
+
+
+
 class Solution {
     public Node connect(Node root) {
-        if(root == null) return null;
+        if (root == null) return null;
+
         Queue<Node> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()) {
-            Node rightNode = null;
-            for(int i = q.size(); i > 0; i--) {
-                Node cur = q.poll();
-                cur.next = rightNode;
-                rightNode = cur;
-                if(cur.right != null) {
-                    q.offer(cur.right);
-                    q.offer(cur.left);
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            Node prev = null;
+
+            for (int i = 0; i < size; i++) {
+                Node node = q.poll();
+
+                if (prev != null) {
+                    prev.next = node;
                 }
+                prev = node;
+
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
             }
+            // last node in the level will have next = null automatically
         }
-        return root;        
+        return root;
     }
 }
