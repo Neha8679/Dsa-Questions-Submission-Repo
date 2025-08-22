fe12a85e6cba875042f167e332c232dc0c22318e
Neha8679 <safercode067@gmail.com>
2025-08-22 15:40:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (95.51%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,26 +13,46 @@
  *     }
  * }
  */
-class Solution {
-    List<Integer>list=new ArrayList<>();
-    public boolean isValidBST(TreeNode root) {
-        inorder(root);
-        int n=list.size();
-        for(int i=0;i<n-1;i++){
-            if(list.get(i)>=list.get(i+1)){
-                return false;
+// class Solution {
+//     List<Integer>list=new ArrayList<>();
+//     public boolean isValidBST(TreeNode root) {
+//         inorder(root);
+//         int n=list.size();
+//         for(int i=0;i<n-1;i++){
+//             if(list.get(i)>=list.get(i+1)){
+//                 return false;
 
-            }
-        }
-        return true;
+//             }
+//         }
+//         return true;
         
+//     }
+//     public void inorder(TreeNode root){
+//         if(root==null){
+//             return ;
+//         }
+//         inorder(root.left);
+//         list.add(root.val);
+//         inorder(root.right);
+//     }
+// }
+
+class Solution {
+    private TreeNode prev;
+
+    public boolean isValidBST(TreeNode root) {
+        prev = null;
+        return inorder(root);
     }
-    public void inorder(TreeNode root){
-        if(root==null){
-            return ;
-        }
-        inorder(root.left);
-        list.add(root.val);
-        inorder(root.right);
+
+    private boolean inorder(TreeNode node) {
+        if (node == null) return true;
+
+        if (!inorder(node.left)) return false;
+
+        if ((prev != null) && (node.val <= prev.val)) return false;
+        prev = node;
+        
+        return inorder(node.right);
     }
 }
