91b00a9fcdc748a30a69923c3de083a427188d49
Neha8679 <safercode067@gmail.com>
2025-09-08 23:41:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (77.34%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -1,5 +1,4 @@
-class Solution {
-    public int[] getNoZeroIntegers(int n) {
+
         // int[] ans=new int[2];
         // if(n%2==0){
         //     ans[0]=1;
@@ -12,14 +11,38 @@ class Solution {
         // return ans;
         
 
-        int[] ans=new int[2];
-        for(int i=1;i<n;i++){
-            int j=n-i;
-            if(!String.valueOf(i).contains("0") && !String.valueOf(j).contains("0")){
-                ans[0]=i;
-                ans[1]=j;
+        // int[] ans=new int[2];
+        // for(int i=1;i<n;i++){
+        //     int j=n-i;
+        //     if(!String.valueOf(i).contains("0") && !String.valueOf(j).contains("0")){
+        //         ans[0]=i;
+        //         ans[1]=j;
+        //     }
+        // }
+        // return ans;
+
+
+class Solution {
+    public int[] getNoZeroIntegers(int n) {
+      
+  
+       for(int i=1;i<=n;i++){
+            int a = i;
+            int b = n-i;
+            if(a>0 && b>0 && iscontainsZero(a) == false && iscontainsZero(b) == false){
+                return new int[]{a,b};
             }
+       }
+    
+    return new int[] {-1,-1};
+    }
+    public boolean iscontainsZero(int n){
+        while(n > 0){
+            int digits = n % 10;
+            if(digits == 0) return true;
+            n /= 10;
         }
-        return ans;
+        return false;
     }
 }
+    
