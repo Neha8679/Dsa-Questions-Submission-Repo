6eebefd27b49e1da2997bd2f9b127e8d7f94f4ca
Neha8679 <safercode067@gmail.com>
2025-09-13 19:55:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Stack using Linked List/stack-using-linked-list.java
+++ Difficulty: Easy/Stack using Linked List/stack-using-linked-list.java
@@ -12,21 +12,23 @@ class MyStack {
     // Function to push an integer into the stack.
     void push(int a) {
         // Add your code here
-        StackNode element=new StackNode(a);
-        element.next=top;
-        top=element;
+        StackNode node=new StackNode(a);
+        node.next=top;
+        top=node;
         
     }
 
     // Function to remove an item from top of the stack.
     int pop() {
-        // Add your code here
+        // Add your code her
         if(top==null){
             return -1;
         }
-        int topData=top.data;
+        int val=top.data;
         StackNode temp=top;
         top=top.next;
-        return topData;
+        
+        return val;
+        
     }
 }
