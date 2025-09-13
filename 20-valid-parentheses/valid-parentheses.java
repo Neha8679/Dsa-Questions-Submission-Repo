d3ed3d35500391e5f6d241b0426a60b3dd74809a
Neha8679 <safercode067@gmail.com>
2025-09-14 02:27:23 +0530

    Time: 1 ms (99.57%) | Memory: 41.7 MB (73.51%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,36 +1,68 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack=new Stack<>();
+        // Stack<Character> stack=new Stack<>();
+        // int n=s.length();
+        // if(n%2!=0){
+        //     return false;
+        // }
+        // for(int i=0;i<n;i++){
+        //     char ch=s.charAt(i);
+            
+        //     if(ch=='(' || ch== '[' || ch=='{'){
+        //         stack.push(ch);
+        //     }else{
+        //     if(stack.isEmpty()){
+        //         return false;
+        //     }
+        //     else if(ch==')' && stack.peek()=='('){
+        //         stack.pop();
+        //     }
+        //     else if(ch=='}' && stack.peek()== '{'){
+        //         stack.pop();
+        //     }
+        //     else if(ch==']' && stack.peek()=='['){
+        //         stack.pop();
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        //     }
+
+        // }
+        
+        // return stack.isEmpty();
+        
+
         int n=s.length();
         if(n%2!=0){
             return false;
         }
-        for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
-            
-            if(ch=='(' || ch== '[' || ch=='{'){
-                stack.push(ch);
-            }else{
-            if(stack.isEmpty()){
-                return false;
+        int[] stack=new int[n];
+        int cnt=0;
+        for(char ch:s.toCharArray()){
+            switch(ch){
+                case '(':
+                case '{':
+                case '[':
+                stack[cnt++]=ch;
+                break;
+                case ')':
+                if(cnt==0 || stack[--cnt]!='('){
+                    return false;
+                }
+                break;
+                case '}':
+                if(cnt==0 || stack[--cnt]!='{'){
+                    return false;
+                }
+                break;
+                case ']':
+                if(cnt==0 || stack[--cnt]!='['){
+                    return false;
+                }
+                break;
             }
-            else if(ch==')' && stack.peek()=='('){
-                stack.pop();
-            }
-            else if(ch=='}' && stack.peek()== '{'){
-                stack.pop();
-            }
-            else if(ch==']' && stack.peek()=='['){
-                stack.pop();
-            }
-            else{
-                return false;
-            }
-            }
-
         }
-        
-        return stack.isEmpty();
-        
+        return cnt==0;
     }
 }
