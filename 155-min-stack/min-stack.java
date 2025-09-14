0d41061f89cca657e6263e65d8425979872c0aa7
Neha8679 <safercode067@gmail.com>
2025-09-14 12:03:15 +0530

    Time: 3 ms (100.00%) | Memory: 44.7 MB (87.78%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,83 +1,58 @@
 class MinStack {
-    // public Stack<Integer> stack;
-    // public Stack<Integer> stack2;
 
-    public Stack<Integer> stack;
-    public Stack<Integer> minStack;
+    class Node{
+        int val;
+        int minval;
+        Node next;
+        Node(int val,int minval){
+            this.val=val;
+            this.minval=minval;
+            this.next=null;
+        }
+    }
+    Node head;
 
     public MinStack() {
-        // stack=new Stack<>();
-        // stack2=new Stack<>();
-        stack=new Stack<>();
-        minStack=new Stack<>();
-        
-        
+        head=null;
         
     }
     
     public void push(int val) {
-        // if(stack.isEmpty()){
-        //     stack.push(val);
-        // }
-        // else{
-        //     if(stack.peek()<val){
-        //         int x=stack.pop();
-            
-        //         stack.push(val);
-        //         stack.push(x);
-        //     }
-        //     else{
-        //         stack.push(val);
-        //     }
-        // }
-        stack.push(val);
-        if(minStack.isEmpty() || val<=minStack.peek()){
-            minStack.push(val);
+        int minval;
+        if(head==null){
+            minval=val;
         }
-        
+        else{
+            minval=(val<=head.minval) ? val:head.minval;
+        }
+        Node newNode=new Node(val,minval);
+        if(head==null){
+            
+            head=newNode;
+        }
+        else{
+            newNode.next=head;
+            head=newNode;
+
+        }
         
     }
     
     public void pop() {
-        // stack.pop();
-        int x=stack.pop();
-        if(x==minStack.peek()){
-            minStack.pop();
-        }
+        head=head.next;
         
     }
     
     public int top() {
-    //   int n=stack.size();
-    //   for(int i=0;i<n;i++){
-    //     stack2.push(stack.peek());
-    //     stack.pop();
-
-    //   }
-    //   return stack2.peek();
-    if(stack.isEmpty()){
-        return -1;
-    }
-    return stack.peek();
+        return head.val;
         
     }
     
     public int getMin() {
-    //     int n=stack2.size();
-    //   for(int i=0;i<n;i++){
-    //     stack.push(stack2.peek());
-    //     stack2.pop();
-    if(minStack.isEmpty()){
-        return -1;
-    }
-    return minStack.peek();
-
-     }
-    
-
+        return head.minval;
         
     }
-
+}
 
 /**
  * Your MinStack object will be instantiated and called as such:
