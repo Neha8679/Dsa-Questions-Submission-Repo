822fe87a3e474dedff8021f40260849006293504
Neha8679 <safercode067@gmail.com>
2025-09-19 21:24:28 +0530

    Time: 17 ms (44.47%) | Memory: 45.5 MB (99.74%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,22 +1,31 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n=nums.length;
-        int[] result=new int[n];
-        Stack<Integer> stack=new Stack<>();
-        for(int i=0;i<2*n;i++){
-            int currentindex=i%n;
-            while(!stack.isEmpty() && nums[stack.peek()]<nums[currentindex]){
-                result[stack.pop()]=nums[currentindex];
-            }
-            if(i<n){
-                stack.push(currentindex);
-            }
+        int[] ans=new int[2*n];
+        int[] newarr=new int[2*n];
+        Stack<Integer>stack=new Stack<>();
+        for(int i=0;i<n;i++){
+            newarr[i]=nums[i];
+            newarr[i+n]=nums[i];
         }
-        while(!stack.isEmpty()){
-            result[stack.pop()]=-1;
+        for(int i=2*n-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()<=newarr[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                ans[i]=-1;
+            }
+            else{
+                ans[i]=stack.peek();
+            }
+            stack.push(newarr[i]);
+        }
+        int[] result=new int[n];
+
+        for(int i=0;i<n;i++){
+            result[i]=ans[i];
         }
         return result;
-
         
     }
 }
