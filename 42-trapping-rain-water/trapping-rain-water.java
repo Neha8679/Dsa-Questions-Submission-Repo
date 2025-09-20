a2cf850fe1eaa4581ecd31e41ccc1d6dae870040
Neha8679 <safercode067@gmail.com>
2025-09-20 11:57:01 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (29.05%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,54 +1,33 @@
 class Solution {
     public int trap(int[] height) {
-        // int total=0;
-        // int n=height.length;
-        // int[] prefixmax=new int[n];
-        // prefixmax[0]=height[0];
-        // for(int i=1;i<n;i++){
-        //     prefixmax[i]=Math.max(prefixmax[i-1],height[i]);
-        // }
-        // int[] suffixmax=new int[n];
-        // suffixmax[n-1]=height[n-1];
-        // for(int i=n-2;i>=0;i--){
-        //     suffixmax[i]=Math.max(suffixmax[i+1],height[i]);
-        // }
-        // for(int i=0;i<n;i++){
-        //     if(height[i]<prefixmax[i] && height[i]<suffixmax[i]){
-        //         total+=Math.min(prefixmax[i],suffixmax[i])-height[i];
-        //     }
-        // }
-        // return total; 
-
+        int n=height.length;
         int leftmax=0;
         int rightmax=0;
+        int l=0;
+        int r=n-1;
         int total=0;
-        int left=0;
-        int n=height.length;
-        int right=n-1;
-        while(left<right){
-            if(height[left]<=height[right]){
-                if(height[left]<leftmax){
-                    total+=(leftmax-height[left]);
+        while(l!=r){
+            if(height[l]<=height[r]){
+                if(leftmax>height[l]){
+                    total +=leftmax-height[l];
                 }
                 else{
-                    leftmax=height[left];
-                   
+                    leftmax=height[l];
                 }
-                left++;
+                l++;
             }
             else{
-                if(height[right]<rightmax){
-                    total+=(rightmax-height[right]);
+                if(rightmax>height[r]){
+                    total +=rightmax-height[r];
                 }
                 else{
-                    rightmax=height[right];
-                    
+                    rightmax=height[r];
                 }
-                right--;
+                r--;
             }
+
         }
         return total;
         
     }
-
 }
