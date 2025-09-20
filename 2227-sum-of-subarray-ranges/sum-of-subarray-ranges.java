00ac3b81c8ba6856ff80da89fc71fd0c0bd904fc
Neha8679 <safercode067@gmail.com>
2025-09-20 18:36:50 +0530

    Time: 24 ms (46.23%) | Memory: 45.1 MB (14.59%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,54 +1,54 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        long smallestsum=0;
-        long largestsum=0;
-        Stack <Integer> stack=new Stack<>();
         int n=nums.length;
-        int[] lefts=new int[n];
-        int[] rights=new int[n];
-        int[] leftl=new int[n];
-        int[] rightl=new int[n];
+        long largestsum=0;
+        long smallestsum=0;
+        int[] psee=new int[n];
+        int[] nse=new int[n];
+        int[] pgee=new int[n];
+        int[] nge=new int[n];
+        Stack<Integer> s=new Stack<>();
         for(int i=0;i<n;i++){
-            while(!stack.isEmpty() && nums[i]>=nums[stack.peek()]){
-                stack.pop();
+            while(!s.isEmpty() && nums[s.peek()]>=nums[i]){
+                s.pop();
             }
-            leftl[i]=stack.isEmpty()? -1:stack.peek();
-            stack.push(i);
+            psee[i]=s.isEmpty() ? -1:s.peek();
+            s.push(i);
 
         }
-        stack.clear();
+        s.clear();
         for(int i=n-1;i>=0;i--){
-            while(!stack.isEmpty() && nums[i]>nums[stack.peek()]){
-                stack.pop();
+            while(!s.isEmpty() && nums[s.peek()]>nums[i]){
+                s.pop();
             }
-            rightl[i]=stack.isEmpty()? n:stack.peek();
-            stack.push(i);
+            nse[i]=s.isEmpty()?n:s.peek();
+            s.push(i);
         }
-        stack.clear();
+        s.clear();
         for(int i=0;i<n;i++){
-            largestsum += (long)nums[i]*(rightl[i]-i)*(i-leftl[i]);
+            smallestsum +=(long)(i-psee[i]) *( nse[i] -i)* nums[i];
         }
-         for(int i=0;i<n;i++){
-            while(!stack.isEmpty() && nums[i]<=nums[stack.peek()]){
-                stack.pop();
+        for(int i=0;i<n;i++){
+            while(!s.isEmpty() && nums[s.peek()]<=nums[i]){
+                s.pop();
             }
-            lefts[i]=stack.isEmpty()? -1:stack.peek();
-            stack.push(i);
+            pgee[i]=s.isEmpty()?-1:s.peek();
+            s.push(i);
+        }
+        s.clear();
+        for(int i=n-1;i>=0;i--){
+            while(!s.isEmpty() && nums[s.peek()]<nums[i]){
+                s.pop();
+            }
+            nge[i]=s.isEmpty() ? n: s.peek();
+            s.push(i);
 
         }
-        stack.clear();
-        for(int i=n-1;i>=0;i--){
-            while(!stack.isEmpty() && nums[i]<nums[stack.peek()]){
-                stack.pop();
-            }
-            rights[i]=stack.isEmpty()? n:stack.peek();
-            stack.push(i);
-        }
-        stack.clear();
+        s.clear();
         for(int i=0;i<n;i++){
-            smallestsum += (long)nums[i]*(rights[i]-i)*(i-lefts[i]);
+            largestsum +=(long)(i-pgee[i]) *(nge[i]-i) * nums[i];
         }
-        return (largestsum-smallestsum);
+        return (largestsum - smallestsum);
 
         
     }
