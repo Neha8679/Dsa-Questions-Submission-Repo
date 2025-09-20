b0f991808f247121b58437a16e548c37d408e0df
Neha8679 <safercode067@gmail.com>
2025-09-20 13:25:09 +0530

    Time: 74 ms (24.89%) | Memory: 51.6 MB (42.10%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,38 +1,47 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int MOD = 1000000007;
-        int n = arr.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        Stack<Integer> stack = new Stack<>();
+        int[] nse=findnse(arr);
+        int[] psee=findpsee(arr);
+        long total=0;
+        int left=0;
+        int right=0;
+        int n=arr.length;
+        long mod=1000000007L;
+        for(int i=0;i<n;i++){
+            left=i-psee[i];
+            right=nse[i]-i;
+            total=(total +((long)left*right *arr[i])%mod)%mod;
 
-        // Calculate left boundaries
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
-                stack.pop();
-            }
-            left[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(i);
         }
+        return (int)total;
 
-        stack.clear();
-
-        // Calculate right boundaries
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
-                stack.pop();
-            }
-            right[i] = stack.isEmpty() ? n : stack.peek();
-            stack.push(i);
-        }
-
-        long total = 0;
-        for (int i = 0; i < n; i++) {
-            total += (long) arr[i] * (i - left[i]) * (right[i] - i);
-            total %= MOD;
-        }
-
-        return (int) total;
         
+    }
+    public int[] findnse(int[] arr){
+        int n=arr.length;
+        int[] nse=new int[n];
+        Stack<Integer>stack=new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
+                stack.pop();
+            }
+            nse[i]=stack.isEmpty() ? n:stack.peek();
+            stack.push(i);
+
+        }
+        return nse;
+    }
+    public int[] findpsee(int[] arr){
+        int n=arr.length;
+        int[] psee=new int[n];
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && arr[stack.peek()]>arr[i]){
+                stack.pop();
+            }
+            psee[i]=stack.isEmpty() ? -1:stack.peek();
+            stack.push(i);
+        }
+        return psee;
     }
 }
