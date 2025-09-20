68760850fab253e87ee8e06c82b9560f64589a4a
Neha8679 <safercode067@gmail.com>
2025-09-20 17:59:57 +0530

    Time: 7 ms (40.89%) | Memory: 45.6 MB (20.08%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,31 +1,32 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        List<Integer> list=new ArrayList<>();
         int n=asteroids.length;
+        Stack<Integer>s=new Stack<>();
+        
         for(int i=0;i<n;i++){
             if(asteroids[i]>0){
-                list.add(asteroids[i]);
+                s.push(asteroids[i]);
             }
             else{
-                while(!list.isEmpty() && list.get(list.size()-1)>0 && list.get(list.size()-1)<Math.abs(asteroids[i])){
-                    list.remove(list.size()-1);
+                while(!s.isEmpty() && Math.abs(asteroids[i])>s.peek() &&s.peek()>0){
+                    s.pop();
                 }
-                if(!list.isEmpty() && list.get(list.size()-1)==Math.abs(asteroids[i]) && list.get(list.size()-1)>0){
-                    list.remove(list.size()-1);
-                  
-                   
+                if(!s.isEmpty() && s.peek()==Math.abs(asteroids[i])){
+                    s.pop();
                 }
-                else if(list.isEmpty() || list.get(list.size()-1)<0){
-                    list.add(asteroids[i]);
-                }
-            }
+                else if(s.isEmpty() || s.peek()<0){
+                    s.push(asteroids[i]);
 
+                }
+                
+            }
         }
-        int[] result=new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            result[i]=list.get(i);
+        int[] ans=new int[s.size()];
+        for(int i=s.size()-1;i>=0;i--){
+            ans[i]=s.peek();
+            s.pop();
         }
-        return result;
+        return ans;
         
     }
 }
