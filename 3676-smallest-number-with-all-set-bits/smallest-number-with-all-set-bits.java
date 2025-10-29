1908f5bfb8b0ef8b7edd58bca4048133810863ec
Neha8679 <safercode067@gmail.com>
2025-10-30 02:04:03 +0530

    Time: 8 ms (4.20%) | Memory: 41.1 MB (16.95%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
@@ -7,15 +7,7 @@ class Solution {
             n=n/2;
         }
         int m=s.length();
-        String ans="";
-        for(int i=0;i<m;i++){
-
-            char ch=s.charAt(i);
-            if(ch=='0'){
-                ch='1';
-            }
-            ans=ans+ch;
-        }
+        
         int z=0;
         for(int j=0;j<m;j++){
             z=z+(int)Math.pow(2,j);
