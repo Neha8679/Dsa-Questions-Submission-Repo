3527be9875d5e6c4aee788f9cf3d463340f4c16d
Neha8679 <safercode067@gmail.com>
2025-10-31 00:15:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (47.31%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans=1.0;
+        long N=n;
+        if(N<0){
+            N=-N;
+        }
+        while(N>0){
+            if(N%2==1){
+                ans=ans*x;
+                N=N-1;
+            }
+            else{
+                x=x*x;
+                N=N/2;
+            }
+        }
+        if(n<0){
+            ans=1.0/ans;
+        }
+        return ans;
+
+        
+    }
+}
