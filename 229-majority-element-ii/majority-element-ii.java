a2c2057d46a0b4c3d52cd0401d6a2ee1ef626080
Neha8679 <safercode067@gmail.com>
2025-10-31 14:14:51 +0530

    Time: 2 ms (99.79%) | Memory: 52.6 MB (5.08%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,42 +1,49 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n=nums.length;
-        int x=0;
-        if(n%3==0){
-            x=n/3;
-
-        }
-        else{
-            x=(n/3)+1;
-        }
-        boolean[] m=new boolean[n];
-       List<Integer> ans=new ArrayList<>();
+        int cnt1=0;
+        int cnt2=0;
+        int ele1=Integer.MIN_VALUE;
+        int ele2=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-            if(m[i]){
-                continue;
+            if(cnt1==0 && ele2!=nums[i]){
+                cnt1++;
+                ele1=nums[i];
             }
-            int cnt=1;
-            m[i]=true;
-
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==nums[j]){
-                    cnt++;
-                    m[j]=true;
-                    
-                }
-                
-
-            }if(n%3==0){
-                if(cnt>x){
-                    ans.add(nums[i]);
-                }
-            }else{
-            if(cnt>=x){
-                ans.add(nums[i]);
+            else if(cnt2==0 && ele1!=nums[i]){
+                cnt2++;
+                ele2=nums[i];
             }
+            else if(ele1==nums[i]){
+                cnt1++;
+            }
+            else if(ele2==nums[i]){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
+        List<Integer> list=new ArrayList<>();
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==ele1){
+                cnt1++;
+            }
+            if(nums[i]==ele2){
+                cnt2++;
+            }
+        }
+        int x=(int)(n/3)+1;
+        if(x<=cnt1){
+            list.add(ele1);
+        }
+        if(x<=cnt2){
+            list.add(ele2);
+        }
+        return list;
         
-        return ans;
     }
 }
