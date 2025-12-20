932c81959a852c62f238ac112e2c3ac2fbcbd5c8
Neha8679 <safercode067@gmail.com>
2025-12-20 23:26:19 +0530

    Time: 8 ms (74.40%) | Memory: 47.3 MB (13.28%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
@@ -1,35 +1,17 @@
 class Solution {
-    public int minDeletionSize(String[] strs) {
-        int m=strs.length;
-        int n=strs[0].length();
-        String[] arr=new String[n];
-        String s1="";
-        
-        for(int i=0;i<n;i++){
-             s1="";
-            for(int j=0;j<m;j++){
-                String s=strs[j];
-                 s1=s1+s.charAt(i);
+    public int minDeletionSize(String[] s) {
+        int n = s.length, m = s[0].length();
+        int count = 0;
 
-            }
-            arr[i]=s1;
-
-        }
-        int k=arr.length;
-        int cnt=0;
-        int l=arr[0].length();
-        for(int i=0;i<k;i++){
-            for(int j=0;j<l-1;j++){
-                String s2=arr[i];
-                if(s2.charAt(j)>s2.charAt(j+1)){
-                    cnt++;
+        for (int row_concrete = 0; row_concrete < m; row_concrete++) {
+            for (int row = 1; row < n; row++) {
+                if (s[row].charAt(row_concrete) < s[row - 1].charAt(row_concrete)) {
+                    count++;
                     break;
-
                 }
-
             }
         }
-        return cnt;
-        
+
+        return count;
     }
 }
