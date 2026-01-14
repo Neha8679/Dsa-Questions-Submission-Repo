01c124aa684a14212ada6bbe213bf755fe24b2af
Neha8679 <safercode067@gmail.com>
2026-01-14 17:47:41 +0530

    Time: 59 ms (10.57%) | Memory: 131 MB (70.14%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,15 +1,13 @@
 class LRUCache {
     Node head=new Node(0,0);
     Node tail=new Node(0,0);
-    Map<Integer,Node> map=new HashMap<>();
+    HashMap<Integer,Node> map=new HashMap<>();
     int cap;
 
     public LRUCache(int capacity) {
         cap=capacity;
         head.next=tail;
         tail.prev=head;
-
-        
     }
     
     public int get(int key) {
@@ -17,29 +15,23 @@ class LRUCache {
             Node node=map.get(key);
             remove(node);
             insert(node);
-            return node.val;
+            return node.value;
         }
         return -1;
-
         
     }
     
     public void put(int key, int value) {
         if(map.containsKey(key)){
-            remove(map.get(key));
+            Node node=map.get(key);
+            remove(node);
+
         }
         if(map.size()==cap){
             remove(tail.prev);
-        }
-        insert(new Node(key,value) );
-        
-    }
-    private void remove(Node node){
-        map.remove(node.key);
-        node.prev.next=node.next;
-        node.next.prev=node.prev;
-   
 
+        }
+        insert(new Node(key,value));
         
     }
     private void insert(Node node){
@@ -49,14 +41,19 @@ class LRUCache {
         head.next=node;
         node.prev=head;
     }
+    private void remove(Node node){
+        map.remove(node.key);
+        node.next.prev=node.prev;
+        node.prev.next=node.next;
+    }
     class Node{
         int key;
-        int val;
+        int value;
         Node prev;
         Node next;
-        Node(int _key,int _val){
+        Node(int _key,int _value){
             key=_key;
-            val=_val;
+            value=_value;
         }
     }
 }
