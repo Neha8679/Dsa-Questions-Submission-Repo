981fd077f793599691eac4ecededa4ff5f42f99f
Neha8679 <safercode067@gmail.com>
2026-03-19 00:16:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (47.10%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length();
+        int cnt = 0;
+
+       
+        int i = n - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+      
+        while (i >= 0 && s.charAt(i) != ' ') {
+            cnt++;
+            i--;
+        }
+
+        return cnt;
+    }
+}
