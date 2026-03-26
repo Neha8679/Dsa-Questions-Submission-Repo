3d91e898ca630b7247dbb7bf63cffe78c812f5f7
Neha8679 <safercode067@gmail.com>
2026-03-26 17:13:16 +0530

    Time: 1 ms (97.52%) | Memory: 42.8 MB (34.45%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -3,11 +3,23 @@ class Solution {
         if(x<2){
             return x;
         }
-        long i=0;
-        while(i*i<=x){
-            i++;
+        long left=0;
+        long right=x;
+        while(left<=right){
+            long mid=left+(right-left)/2;
+            long square=mid*mid;
+            if(square==x){
+                return (int)mid;
+            }
+            else if(square<x){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
         }
-        return (int)(i-1);
+        return (int)right;
+
         
     }
 }
